2d990361003cce094915e9bbc9e541596e758f08
Demeke Getaneh <86113690+gitdeme@users.noreply.github.com>
2026-09-04 06:49:35 +0300

    config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,41 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   images: {
     localPatterns: [
       { pathname: "/images/**" },
       { pathname: "/leadership/**" },
       { pathname: "/hospital-logo.*" },
     ],
+  },
+   async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value:
+              process.env.NODE_ENV === "production"
+                ? "https://api.medhinhospital.com/"
+                : "http://localhost:3000",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+        ],
+      },
+    ];
   },
 };
 
